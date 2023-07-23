0f196cd56003b6a7b419526052e6897eba47a726
Concerned-Doggo <codeformetoo@proton.me>
2023-07-23 10:36:29 +0530

    add app.js and basic word ejs with video template

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const _ = require("lodash");
+const ejs = require("ejs");
+
+const app = express();
+
+
+app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+
+
+app.get("/", (req, res)=>{
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/word/:customeUrl", (req, res)=>{
+  const word = req.params.customeUrl;
+  res.render("word", {word: word});
+});
+
+
+
+
+
+
+
+
+
+app.listen(8080, (req, res)=>{
+  console.log("server is runnig on port 8080");
+})
