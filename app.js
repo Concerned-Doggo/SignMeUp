cbfedc444b02813bdb7683e3f34d4b42f4ffde83
Concerned-Doggo <codeformetoo@proton.me>
2023-07-30 18:42:31 +0530

    update letters, aboutus page + add group(GOL) page + add a-f,g,l videos + add all letter-icons

--- app.js
+++ app.js
@@ -13,11 +13,12 @@ app.set('view engine', 'ejs');
 
 
 app.get("/", (req, res)=>{
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/public/html/index.html");
 });
 
 app.get("/letter", (req, res)=>{
-  res.render("letter",{});
+  const letter = ['a','f','g','l','m','r','s','z']
+  res.render("letter",{letter: letter});
 });
 
 app.get("/feedback", (req, res)=>{
@@ -29,9 +30,28 @@ app.get("/aboutus", (req, res)=>{
 });
 
 app.get("/letter/:customeUrl", (req, res)=>{
-  const word = req.params.customeUrl;
-  res.render("word", {word: word});
+  const customeUrl = req.params.customeUrl;
+  let letterArray = [];
+  switch (customeUrl){
+    case 'a-f':
+      letterArray = ['a','b','c','d','e','f'];
+      break;
+    case 'g-l':
+      letterArray = ['g','h','i','j','k','l'];
+      break;
+    case 'm-r':
+      letterArray = ['m','n','o','p','q','r'];
+      break;
+    case 's-z':
+      letterArray = ['s','t','u','v','w','x','y','z'];
+  }
+
+
+  res.render("GOL", {letterArray: letterArray});
 });
+// app.get("/letter/:customUrl", (req, res)=>{
+//   res.render("GOL");
+// });
 
 app.get("/attributions", (req, res)=>{
   res.sendFile(__dirname + "/attributions.html");
