d7ba926e2c65b3b326e3f4593f3b31dfd3890679
Concerned-Doggo <codeformetoo@proton.me>
2023-09-12 21:59:46 +0530

    update css color

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const dotenv = require('dotenv').config()
+const dotenv = require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const _ = require("lodash");
@@ -7,25 +7,25 @@ const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 
-
 const app = express();
 const port = 8080;
 
-
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(session({
-  secret: process.env["SECRET"],
-  resave: false,
-  saveUninitialized: false,
-}));
+app.use(
+  session({
+    secret: process.env["SECRET"],
+    resave: false,
+    saveUninitialized: false,
+  }),
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect('mongodb://127.0.0.1/SignMeUp')
+mongoose.connect("mongodb://127.0.0.1/SignMeUp");
 
 // const questionSchema = mongoose.Schema({
 //    question: String,
@@ -48,11 +48,9 @@ userSchema.plugin(passportLocalMongoose);
 
 const User = mongoose.model("User", userSchema);
 
-
-
 const blogSchema = mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  blog : {
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  blog: {
     title: String,
     body: String,
   },
@@ -64,21 +62,20 @@ const blog1 = new Blog({
   userId: "64f6bf84fa57f3e023501af6",
   blog: {
     title: "blog1",
-    body: "Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis."
-  }
- });
+    body: "Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.",
+  },
+});
 
 const blog2 = new Blog({
   userId: "64f55586bda46f549ceeb82c",
   blog: {
     title: "Lorem Ipsum Blog",
-    body: "Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis."
-  }
-})
+    body: "Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.",
+  },
+});
 // blog2.save();
 // model for question schema
 // const Question = new mongoose.model("Question", questionSchema);
-
 
 // inititalizing passport for our User model
 passport.use(User.createStrategy());
@@ -86,172 +83,176 @@ passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
-
 const isLoggedIn = (req) => {
   let loggedIn = 0;
-  req.isAuthenticated() ? loggedIn = 1 : loggedIn = 0;
+  req.isAuthenticated() ? (loggedIn = 1) : (loggedIn = 0);
 
-  let username = ""; 
-  if(req.user) username = req.user.username;
-}
-
-
+  let username = "";
+  if (req.user) username = req.user.username;
+};
 
 const switchFunc = function (customUrl) {
   letterArray = [];
   switch (customUrl) {
-    case 'a-f':
-      letterArray = ['a', 'b', 'c', 'd', 'e', 'f'];
+    case "a-f":
+      letterArray = ["a", "b", "c", "d", "e", "f"];
       break;
-    case 'g-l':
-      letterArray = ['g', 'h', 'i', 'j', 'k', 'l'];
+    case "g-l":
+      letterArray = ["g", "h", "i", "j", "k", "l"];
       break;
-    case 'm-r':
-      letterArray = ['m', 'n', 'o', 'p', 'q', 'r'];
+    case "m-r":
+      letterArray = ["m", "n", "o", "p", "q", "r"];
       break;
-    case 's-z':
-      letterArray = ['s', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    case "s-z":
+      letterArray = ["s", "t", "u", "v", "w", "x", "y", "z"];
   }
-  return letterArray
-}
+  return letterArray;
+};
 const shuffle = function (array) {
-  let currentIndex = array.length,  randomIndex;
+  let currentIndex = array.length,
+    randomIndex;
 
   // While there remain elements to shuffle.
   while (currentIndex != 0) {
-
     // Pick a remaining element.
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;
 
     // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
   }
 
   return array;
-}
+};
 
 let letterArray = [];
 let options = [];
-const generateOptions = function(array, options){
+const generateOptions = function (array, options) {
   // (i + constatn) % array.length
 
   let n = array.length;
-  for(let i = 0; i < n; i++){
+  for (let i = 0; i < n; i++) {
     let optionRow = [
-      {text: 'a', correct: false},
-      {text: 'a', correct: false},
-      {text: 'a', correct: false}
+      { text: "a", correct: false },
+      { text: "a", correct: false },
+      { text: "a", correct: false },
     ];
-    optionRow[0] = {text: array[i], correct: true}; 
-    optionRow[1] = {text: array[(i+4) % n], correct: false};
-    optionRow[2] = {text: array[(i+5) % n], correct: false};
+    optionRow[0] = { text: array[i], correct: true };
+    optionRow[1] = { text: array[(i + 4) % n], correct: false };
+    optionRow[2] = { text: array[(i + 5) % n], correct: false };
     shuffle(optionRow);
     options.push(optionRow);
   }
   return options;
-}
+};
 
-generateOptions([1],[1]);
-
+generateOptions([1], [1]);
 
 app.get("/", (req, res) => {
   let loggedIn = 0;
 
-  let username = ""; 
-  if(req.user) username = req.user.username;
+  let username = "";
+  if (req.user) username = req.user.username;
 
-  req.isAuthenticated() === true ? loggedIn = 1: loggedIn = 0;
-  
-  res.render("index", {username: username, loggedIn: loggedIn});
+  req.isAuthenticated() === true ? (loggedIn = 1) : (loggedIn = 0);
+
+  res.render("index", { username: username, loggedIn: loggedIn });
 });
-
-
-
-
 
 app.get("/letter", (req, res) => {
   let loggedIn = 0;
-  req.isAuthenticated() ? loggedIn = 1 : loggedIn = 0;
-  const letter = ['a', 'f', 'g', 'l', 'm', 'r', 's', 'z']
+  req.isAuthenticated() ? (loggedIn = 1) : (loggedIn = 0);
+  const letter = ["a", "f", "g", "l", "m", "r", "s", "z"];
 
-  let username = ""; 
-  if(req.user) username = req.user.username;
+  let username = "";
+  if (req.user) username = req.user.username;
 
-  res.render("letter", { letter: letter, loggedIn: loggedIn, username: username});
+  res.render("letter", {
+    letter: letter,
+    loggedIn: loggedIn,
+    username: username,
+  });
 });
 
 app.get("/feedback", (req, res) => {
   let loggedIn = 0;
-  req.isAuthenticated() ? loggedIn = 1 : loggedIn = 0;
+  req.isAuthenticated() ? (loggedIn = 1) : (loggedIn = 0);
 
-  let username = ""; 
-  if(req.user) username = req.user.username;
+  let username = "";
+  if (req.user) username = req.user.username;
 
-
-  res.render("feedback", {loggedIn: loggedIn, username: username});
+  res.render("feedback", { loggedIn: loggedIn, username: username });
 });
 
-app.post("/feedback", (req, res) =>{
+app.post("/feedback", (req, res) => {
   console.log(req.body.email);
   console.log(req.body.feed);
-})
+});
 
 app.get("/aboutus", (req, res) => {
   let loggedIn = 0;
-  req.isAuthenticated() ? loggedIn = 1 : loggedIn = 0;
- 
-  let username = ""; 
-  if(req.user) username = req.user.username;
-  
-  const profile = [{
-    name: "Harshal Chavan",
-    github: "Concerned-Doggo ",
-    linkedIn: "harshal-chavan-20129324b",
-    }, 
+  req.isAuthenticated() ? (loggedIn = 1) : (loggedIn = 0);
+
+  let username = "";
+  if (req.user) username = req.user.username;
+
+  const profile = [
     {
-    name: "Ajinkya Birari",
-    github: "awkward-raccoon",
-    linkedIn: "ajinkya-birari-25566b229",
+      name: "Harshal Chavan",
+      github: "Concerned-Doggo ",
+      linkedIn: "harshal-chavan-20129324b",
+    },
+    {
+      name: "Ajinkya Birari",
+      github: "awkward-raccoon",
+      linkedIn: "ajinkya-birari-25566b229",
     },
     {
       name: "Abhay Pandey",
-    github: "AP2290",
-    linkedIn: "abhay-pandey-583337269",
+      github: "AP2290",
+      linkedIn: "abhay-pandey-583337269",
     },
     {
       name: "Atharva Aurangabadkar",
-    github: "Atharva0825",
-    linkedIn: "atharv-aurangabadkar-a24373289",
-    }];
+      github: "Atharva0825",
+      linkedIn: "atharv-aurangabadkar-a24373289",
+    },
+  ];
 
- res.render("aboutus", {loggedIn: loggedIn, username: username, profile: profile});
+  res.render("aboutus", {
+    loggedIn: loggedIn,
+    username: username,
+    profile: profile,
+  });
 });
 
 app.get("/letter/:customeUrl", (req, res) => {
   let loggedIn = 0;
-  req.isAuthenticated() ? loggedIn = 1 : loggedIn = 0;
+  req.isAuthenticated() ? (loggedIn = 1) : (loggedIn = 0);
 
   const customUrl = req.params.customeUrl;
 
   const letterArray = switchFunc(customUrl);
 
-  let username = ""; 
-  if(req.user) username = req.user.username;
+  let username = "";
+  if (req.user) username = req.user.username;
 
-
-  res.render("GOL", { letterArray: letterArray , loggedIn: loggedIn, username: username});
+  res.render("GOL", {
+    letterArray: letterArray,
+    loggedIn: loggedIn,
+    username: username,
+  });
 });
 
 app.get("/attributions", (req, res) => {
- 
-  let username = ""; 
-  if(req.user) username = req.user.username;
+  let username = "";
+  if (req.user) username = req.user.username;
 
- res.render("attributions", {loggedIn: loggedIn, username: username});
+  res.render("attributions", { loggedIn: loggedIn, username: username });
 });
-
 
 // app.get("/quiz/:customUrl", (req, res) => {
 //   const customUrl = req.params.customUrl;
@@ -265,108 +266,117 @@ app.get("/attributions", (req, res) => {
 // });
 //
 
-
-app.get("/register", (req,res) => {
-  
-  res.render("register",{loggedIn: 0, username: ""});
+app.get("/register", (req, res) => {
+  res.render("register", { loggedIn: 0, username: "" });
 });
 
-app.post("/register", (req,res)=>{
-  User.register({username: req.body.username}, req.body.password)
+app.post("/register", (req, res) => {
+  User.register({ username: req.body.username }, req.body.password)
     .then(() => {
-      const authenticate = passport.authenticate('local');
-      authenticate(req, res, ()=>{
+      const authenticate = passport.authenticate("local");
+      authenticate(req, res, () => {
         res.redirect("/");
       });
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       res.redirect("/register");
     });
 });
 
-app.get("/login", (req, res)=>{
-  res.render("login", {username: "", loggedIn: 0});
+app.get("/login", (req, res) => {
+  res.render("login", { username: "", loggedIn: 0 });
 });
 
-app.post('/login',passport.authenticate('local', { failureRedirect: '/login', failureMessage: true }),
-  function(req, res) {
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureMessage: true,
+  }),
+  function (req, res) {
     res.redirect("/");
-  });
+  },
+);
 
 app.get("/user/:customUrl", (req, res) => {
-  
   let loggedIn = 0;
-  req.isAuthenticated() ? loggedIn = 1 : loggedIn = 0;
+  req.isAuthenticated() ? (loggedIn = 1) : (loggedIn = 0);
 
-  let username = ""; 
-  if(req.user) username = req.user.username;
+  let username = "";
+  if (req.user) username = req.user.username;
 
-
-  res.render("user", {username: username, loggedIn: loggedIn});
+  res.render("user", { username: username, loggedIn: loggedIn });
 });
 
-app.post('/logout', function(req, res, next){
-  req.logout(function(err) {
-    if (err) { return next(err); }
-    res.redirect('/');
+app.post("/logout", function (req, res, next) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
   });
 });
 
 app.get("/edit", (req, res) => {
   let loggedIn = 0;
-  req.isAuthenticated() ? loggedIn = 1 : loggedIn = 0;
+  req.isAuthenticated() ? (loggedIn = 1) : (loggedIn = 0);
 
-  let username = ""; 
-  if(req.user) {
+  let username = "";
+  if (req.user) {
     password = req.user.password;
     username = req.user.username;
   }
-  res.render("editProfile",{username: username, loggedIn: loggedIn})
+  res.render("editProfile", { username: username, loggedIn: loggedIn });
 });
 
 app.post("/edit", (req, res) => {
   const newUsername = req.body.newUsername;
   const prevUsername = req.body.username;
-  if(prevUsername != newUsername){
-    User.findOneAndUpdate({username: prevUsername}, {username: newUsername})
-      .then(found =>{
+  if (prevUsername != newUsername) {
+    User.findOneAndUpdate({ username: prevUsername }, { username: newUsername })
+      .then((found) => {
         console.log("done");
-    })
-      .catch(err => console.log(err));
+      })
+      .catch((err) => console.log(err));
   }
 
   res.redirect("/");
-})
+});
 
 app.get("/blog", (req, res) => {
- 
   let loggedIn = 0;
-  req.isAuthenticated() ? loggedIn = 1 : loggedIn = 0;
+  req.isAuthenticated() ? (loggedIn = 1) : (loggedIn = 0);
 
-  let username = ""; 
-  if(req.user) username = req.user.username;
+  let username = "";
+  if (req.user) username = req.user.username;
   Blog.find()
-    .then(blogs => {
+    .then((blogs) => {
       // console.log(blogs)
       // const pblog = Blog.find({})
       // const populatedBlogs = Blog.find({}).populate('userId').exec();
       // console.log("populatedBlogs");
       // console.log(populatedBlogs.userId.username);
-      res.render("blog", {username: username, loggedIn: loggedIn, blogs: blogs});
+      res.render("blog", {
+        username: username,
+        loggedIn: loggedIn,
+        blogs: blogs,
+      });
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 });
 
 app.get("/text-to-sign", (req, res) => {
-  res.render("text-to-sign", {username: "", loggedIn: 0, inputs: ""})
-})
+  res.render("text-to-sign", { username: "", loggedIn: 0, inputs: "" });
+});
 app.post("/text-to-sign", (req, res) => {
-
-  res.render("text-to-sign", {username: "", loggedIn: 0, inputs: req.body.input});
-})
-
+  res.render("text-to-sign", {
+    username: "",
+    loggedIn: 0,
+    inputs: req.body.input,
+  });
+});
 
 app.listen(port, (req, res) => {
   console.log(`server is runnig on port ${port}`);
-})
+});
